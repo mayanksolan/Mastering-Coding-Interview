49abd4d25722c8935c2345dcc49bef779d88b6bd
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-31 00:41:26 +0530

    descriptions pseudocode

--- Coding-Interview/DataStructures/MergeSortMergingArrays.js
+++ Coding-Interview/DataStructures/MergeSortMergingArrays.js
@@ -1,3 +1,15 @@
+// Merging Arrays Pseudocode
+// Create an empty array, take a look at the smallest values in each input array
+// While there are still values we haven't looked at...
+// If the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
+// If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
+// Once we exhaust one array, push in all remaining values from the other array
+
+// mergeSort Pseudocode
+// Break up the array into halves until you have arrays that are empty or have one element
+// Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+// Once the array has been merged back together, return the merged (and sorted!) array
+
 function merge(arr1, arr2) {
   let res = [];
   let i = 0,
--- Coding-Interview/DataStructures/QuickSort.js
+++ Coding-Interview/DataStructures/QuickSort.js
@@ -1,3 +1,17 @@
+// Pivot Pseudocode
+// It will help to accept three arguments: an array, a start index, and an end index (these can default to 0 and the array length minus 1, respectively)
+// Grab the pivot from the start of the array
+// Store the current pivot index in a variable (this will keep track of where the pivot should end up)
+// Loop through the array from the start until the end
+// If the pivot is greater than the current element, increment the pivot index variable and then swap the current element with the element at the pivot index
+// Swap the starting element (i.e. the pivot) with the pivot index
+// Return the pivot index
+
+// Quicksort Pseudocode
+// Call the pivot helper on the array
+// When the helper returns to you the updated pivot index, recursively call the pivot helper on the subarray to the left of that index, and the subarray to the right of that index
+// Your base case occurs when you consider a subarray with less than 2 elements
+
 let arr = [28, 41, 4, 11, 16, 1, 40, 14, 36, 37, 42, 18];
 
 function pivot(arr, start = 0, end = arr.length - 1) {
